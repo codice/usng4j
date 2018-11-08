39c6dcc3e3f80dbb9664d7c70c1623b0dfb976eb
Dimitry Sherman <dimitry.sherman@connexta.com>
2018-11-08 14:34:40 -0700

    Added initial skeleton for the UtmUpsCoordinateImpl unit tests.

---
+++ usng4j-impl/src/test/java/org.codice.usng4j.impl/UtmUpsCoordinateImplTest.java
@@ -0,0 +1,39 @@
+package org.codice.usng4j.impl;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.fail;
+
+public class UtmUpsCoordinateImplTest {
+
+  @Test
+  public void testSuccessfulParsingUtmString() {
+    fail();
+  }
+
+  @Test
+  public void testParsingInvalidUtmString() {
+    fail();
+  }
+
+  @Test
+  public void testSuccessfulParsingUpsString() {
+    fail();
+  }
+
+  @Test
+  public void testParsingInvalidUpsString() {
+    fail();
+  }
+
+  @Test
+  public void testSuccessfulParsingUtmStringWithOverlappingUpsCoordinates() {
+    fail();
+  }
+
+  @Test
+  public void testSuccessfulParsingUpsStringWithOverlappingUtmCoordinates() {
+    fail();
+  }
+}

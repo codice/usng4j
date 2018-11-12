6d7e933334fcb7952c689f0c89cb9b8e932c05a5
Dimitry Sherman <dimitry.sherman@connexta.com>
2018-11-12 08:47:41 -0700

    Updated `UtmUpsCoordinateImplTest` to read test objects into their own class instances.

--- usng4j-impl/src/test/java/org/codice/usng4j/impl/UtmUpsCoordinateImplTest.java
+++ usng4j-impl/src/test/java/org/codice/usng4j/impl/UtmUpsCoordinateImplTest.java
@@ -2,18 +2,66 @@ package org.codice.usng4j.impl;
 
 import static org.junit.Assert.fail;
 
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class UtmUpsCoordinateImplTest {
 
-  private Gson gson = new GsonBuilder().create();
+  class UtmUpsTestData {
+    final double latitude;
+    final double longitude;
+    final double easting;
+    final double northing;
+    final boolean northPole;
 
-  private InputStreamReader reader(String file) throws IOException {
-    return new InputStreamReader(UtmUpsCoordinateImplTest.class.getResourceAsStream(file), "UTF-8");
+    UtmUpsTestData(
+        final double latitude,
+        final double longitude,
+        final double easting,
+        final double northing,
+        final boolean northPole) {
+      this.latitude = latitude;
+      this.longitude = longitude;
+      this.easting = easting;
+      this.northing = northing;
+      this.northPole = northPole;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Lat: %f,  Lon: %f,  E: %f,  N: %f,  NP: %b",
+        latitude, longitude, easting, northing, northPole);
+    }
+  }
+
+  private static final List<UtmUpsTestData> validUpsCoordinatesTests = new ArrayList<>();
+  private static final List<UtmUpsTestData> validUtmCoordinatesTests = new ArrayList<>();
+
+  private static InputStreamReader reader(final String fileName)
+    throws IOException {
+    return new InputStreamReader(UtmUpsCoordinateImplTest.class.getResourceAsStream(fileName), "UTF-8");
+  }
+
+  @BeforeClass
+  public static void allTestsSetup() throws IOException {
+    final Gson gson = new GsonBuilder().create();
+    final Type testDataListType = new TypeToken<List<UtmUpsTestData>>() {}.getType();
+    validUpsCoordinatesTests.addAll(gson.fromJson(
+      reader("/ValidUpsCoordinates.json"), testDataListType));
+    // TODO:  add a set of valid UTM coordinates test data
+    // validUtmCoordinatesTests.addAll(gson.fromJson(
+    //  reader("/ValidUtmCoordinates.json"), testDataListType));
+    // TODO:  add a set of invalid UPS and UTM coordinates test data
+    // TODO:  add reading invalid tests
   }
 
   // UTM parsing tests
@@ -42,11 +90,6 @@ public class UtmUpsCoordinateImplTest {
 
   @Test
   public void testParsingUpsAsUps() throws IOException {
-    UtmUpsCoordinateImpl[] upsObjects =
-        gson.fromJson(reader("/ValidUpsCoordinates.json"), UtmUpsCoordinateImpl[].class);
-    for (UtmUpsCoordinateImpl upsCoordinate : upsObjects) {
-      System.out.println(upsCoordinate.toString());
-    }
     fail();
   }
 

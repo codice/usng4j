c1a69aedb2efafd5ce2a4e20402c5579bf4588c7
Dimitry Sherman <dimitry.sherman@connexta.com>
2018-11-12 16:16:42 -0700

    Added `String` representations to the test data `.json` file, and made necessary modifications to the `UtmUpsTestData` class.

--- usng4j-impl/src/test/java/org/codice/usng4j/impl/UtmUpsTestData.java
+++ usng4j-impl/src/test/java/org/codice/usng4j/impl/UtmUpsTestData.java
@@ -1,29 +1,43 @@
 package org.codice.usng4j.impl;
 
+import org.codice.usng4j.NSIndicator;
+
 class UtmUpsTestData {
   final double latitude;
   final double longitude;
   final double easting;
   final double northing;
-  final boolean northPole;
+  final NSIndicator nsIndicator;
+  final String upsString;
+  final String utmString;
+  final String latLonString;
+  final String mgrsString;
 
   UtmUpsTestData(
       final double latitude,
       final double longitude,
       final double easting,
       final double northing,
-      final boolean northPole) {
+      final NSIndicator nsIndicator,
+      final String upsString,
+      final String utmString,
+      final String latLonString,
+      final String mgrsString) {
     this.latitude = latitude;
     this.longitude = longitude;
     this.easting = easting;
     this.northing = northing;
-    this.northPole = northPole;
+    this.nsIndicator = nsIndicator;
+    this.upsString = upsString;
+    this.utmString = utmString;
+    this.latLonString = latLonString;
+    this.mgrsString = mgrsString;
   }
 
   @Override
   public String toString() {
     return String.format(
-        "Lat: %f,  Lon: %f,  E: %f,  N: %f,  NP: %b",
-        latitude, longitude, easting, northing, northPole);
+        "Lat: %f,  Lon: %f,  E: %f,  N: %f,  NP: %s",
+        latitude, longitude, easting, northing, nsIndicator);
   }
 }

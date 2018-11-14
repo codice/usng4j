4bf646f66f6a831b7c2966f0e7e526ce93e80485
Dimitry Sherman <dimitry.sherman@connexta.com>
2018-11-14 16:11:30 -0700

    Added test data iteration code by @Variadicism (slightly generalized for easier re-use).

--- usng4j-impl/src/test/java/org/codice/usng4j/impl/BaseClassForUsng4jTest.java
+++ usng4j-impl/src/test/java/org/codice/usng4j/impl/BaseClassForUsng4jTest.java
@@ -1,5 +1,7 @@
 package org.codice.usng4j.impl;
 
+import static org.junit.Assert.fail;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -8,6 +10,9 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import org.junit.BeforeClass;
 
 public abstract class BaseClassForUsng4jTest {
@@ -30,5 +35,33 @@ public abstract class BaseClassForUsng4jTest {
     //  reader("/ValidUtmCoordinates.json"), testDataListType));
     // TODO:  add a set of invalid UPS and UTM coordinates test data
     // TODO:  add reading invalid tests
+  }
+
+  void runTestWithWithData(
+      final Consumer<UtmUpsTestData> testDataConsumer, final List<UtmUpsTestData> testDataList) {
+    final List<String> errorMessages = runTestsCollectErrors(testDataConsumer, testDataList);
+    if (!errorMessages.isEmpty()) {
+      fail(
+          "Some valid UPS coordinates could not be converted to lat-lon: "
+              + errorMessages.stream().map(String::toString).collect(Collectors.joining("\n")));
+    }
+  }
+
+  private List<String> runTestsCollectErrors(
+      final Consumer<UtmUpsTestData> testDataConsumer, final List<UtmUpsTestData> testDataList) {
+    return testDataList
+        .stream()
+        .map(
+            testData -> {
+              try {
+                testDataConsumer.accept(testData);
+                return Optional.<String>empty();
+              } catch (AssertionError assertionFailure) {
+                return Optional.of(assertionFailure.getMessage());
+              }
+            })
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .collect(Collectors.toList());
   }
 }

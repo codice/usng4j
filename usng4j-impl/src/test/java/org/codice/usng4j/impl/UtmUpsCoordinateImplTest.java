f728253866eeb3e3bb3f2713004068f735725be9
Dimitry Sherman <dimitry.sherman@connexta.com>
2018-11-14 13:07:48 -0700

    Auto stash before rebase of "forked/addingUpsCoordinates"

--- usng4j-impl/src/test/java/org/codice/usng4j/impl/UtmUpsCoordinateImplTest.java
+++ usng4j-impl/src/test/java/org/codice/usng4j/impl/UtmUpsCoordinateImplTest.java
@@ -1,8 +1,12 @@
 package org.codice.usng4j.impl;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import org.codice.usng4j.UtmUpsCoordinate;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class UtmUpsCoordinateImplTest extends BaseClassForUsng4jTest {
@@ -69,12 +73,31 @@ public class UtmUpsCoordinateImplTest extends BaseClassForUsng4jTest {
   }
 
   @Test
-  public void testParsingUtmOverlappingUpsAsUtmUps() {
-    fail();
+  public void testAlwaysFalseIsOverlappingException() {
+    // TODO:  remove this test once the overlap exception functionality is implemented
+    final UtmUpsCoordinate testCoordinate =
+        UtmUpsCoordinateImpl.fromZoneBandNorthingEasting(32, 'X', 425_945, 8_931_452);
+    // When the overlap exception functionality is implemented the following should fail
+    assertThat(UtmUpsCoordinateImpl.isUpsOverlapException(testCoordinate), is(false));
   }
 
+  @Ignore
   @Test
-  public void testParsingUpsOverlappingUtmAsUtmUps() {
-    fail();
+  public void testParsingValidUtmOverlappingUpsAsUtmUps() {
+    // This functionality is not fully implemented yet.
+    // TODO:  update this test once it's implemented 35X 425945mE 8931452mN
+    final UtmUpsCoordinate testCoordinate =
+        UtmUpsCoordinateImpl.fromZoneBandNorthingEasting(32, 'V', 425_945, 8_931_452);
+    assertThat(testCoordinate.isUTM() && testCoordinate.isUPS(), is(true));
+  }
+
+  @Ignore
+  @Test
+  public void testParsingValidUpsOverlappingUtmAsUtmUps() {
+    // This functionality is not fully implemented yet.
+    // TODO:  update this test once it's implemented, and the test data updated from UTM to UPS
+    final UtmUpsCoordinate testCoordinate =
+        UtmUpsCoordinateImpl.fromZoneBandNorthingEasting(32, 'V', 425_945, 8_931_452);
+    assertThat(testCoordinate.isUTM() && testCoordinate.isUPS(), is(true));
   }
 }
